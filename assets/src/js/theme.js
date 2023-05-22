58572616c50023361f2f98072b2ef7023876ea6a
Lukáš Záplata <lukas.zaplata@gmail.com>
2023-05-22 14:38:18 +0200

    theme initialization

---
+++ assets/src/js/theme.js
@@ -0,0 +1,12 @@
+import lightGallery from "lightgallery";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+
+window.lightGallery = lightGallery;
+window.lgThumbnail = lgThumbnail;
+window.lgZoom = lgZoom;
+
+window.$ = window.jQuery = require("jquery");
+
+$(document).ready(function () {
+});
